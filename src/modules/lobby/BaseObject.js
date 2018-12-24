8ae43f7fe736d69a974b2c83d025246e12c175d6
CPU11643_LOCAL <phamkhaclinh2017@gmail.com>
2018-12-24 16:34:24 +0700

    Map init

--- src/modules/lobby/BaseObject.js
+++ src/modules/lobby/BaseObject.js
@@ -2,24 +2,16 @@
  * Created by CPU11643_LOCAL on 12/24/2018.
  */
 
-gv.BUILDING = {}
 
-gv.BUILDING.ARMY_CAMP = 1;
-gv.BUILDING.BARRACK  = 2;
-gv.BUILDING.BUILDER_HUT  = 3;
-gv.BUILDING.CLAN_CASTLE  = 4;
-gv.BUILDING.DEFENSE  = 5;
-gv.BUILDING.LABORATORY  = 6;
-gv.BUILDING.OBSTACLE  = 7;
-gv.BUILDING.RESOURCE  = 8;
-gv.BUILDING.STORAGE  = 9;
-gv.BUILDING.TOWN_HALL  = 10;
+
+
+
 
 var BaseObject = cc.Class.extend({
     id: 0,
     type: null,
-    level: 0,
-    level2: 0,
+    currentLevel: 0,
+    nextLevel: 0,
     PosX: null,
     PosY: null,
     Imagelink: "",
@@ -40,7 +32,36 @@ var BaseObject = cc.Class.extend({
     },
 
     setImage: function () {
+        if (this.type == gv.BUILDING.ARMY_CAMP)   this.Imagelink =  TL.PATH.army_camp + this.currentLevel + "/idle/image0000.png";
 
+        if (this.type == gv.BUILDING.BARRACK)     this.Imagelink =  TL.PATH.barrack + this.currentLevel + "/idle/image0000.png";
+
+        if (this.type == gv.BUILDING.BUILDER_HUT)     this.Imagelink =  TL.PATH.builder_hut + "/idle/image0000.png";
+
+        if (this.type == gv.BUILDING.CLAN_CASTLE)     this.Imagelink =  TL.PATH.clan_castle + this.currentLevel + "/idle/image0000.png";
+
+        if (this.type == gv.BUILDING.DEFENSE)     this.Imagelink =  TL.PATH.defense_base + this.type2 + "_" + this.currentLevel + "_Shadow.png";
+
+        if (this.type == gv.BUILDING.TOWN_HALL)     this.Imagelink =  TL.PATH.town_hall + this.currentLevel + "/idle/image0000.png";
+
+        if (this.type == gv.BUILDING.LABORATORY)     this.Imagelink =  TL.PATH.laboratory + this.currentLevel + "/idle/image0000.png";
+
+        if (this.type == gv.BUILDING.OBSTACLE)     this.Imagelink =  TL.PATH.obstacle + this.type2 + "/idle/image0000.png";
+
+        if (this.type == gv.BUILDING.STORAGE)     {
+            if(type2 == 1) this.Imagelink =  TL.PATH.gold_storgae + this.currentLevel + "/idle/image0000.png";
+
+            if(type2 == 2) this.Imagelink =  TL.PATH.elixir_storage + this.currentLevel + "/idle/image0000.png";
+
+            if(type2 == 3) this.Imagelink =  TL.PATH.dark_elixir_storage + this.currentLevel + "/idle/image0000.png";
+        }
+
+        if (this.type == gv.BUILDING.RESOURCE)     {
+            if(type2 == 1) this.Imagelink =  TL.PATH.gold_mine + this.currentLevel + "/idle/image0000.png";
+
+            if(type2 == 2) this.Imagelink =  TL.PATH.elixir_collector + this.currentLevel + "/idle/image0000.png";
+
+            if(type2 == 3) this.Imagelink =  TL.PATH.dark_elixir_collector + this.currentLevel + "/idle/image0000.png";
+        }
     }
-
 })
--- src/modules/lobby/Path.js
+++ src/modules/lobby/Path.js
@@ -3,8 +3,44 @@
  */
 
 
+gv.BUILDING = {}
+
+gv.BUILDING.ARMY_CAMP = 1; // ok
+gv.BUILDING.BARRACK  = 2;  // ok
+gv.BUILDING.BUILDER_HUT  = 3;  // ok
+gv.BUILDING.CLAN_CASTLE  = 4; // ok
+gv.BUILDING.DEFENSE  = 5;   // load only cai base truoc
+gv.BUILDING.LABORATORY  = 6;  // ok
+gv.BUILDING.OBSTACLE  = 7;
+gv.BUILDING.RESOURCE  = 8;
+gv.BUILDING.STORAGE  = 9;  // da thay
+gv.BUILDING.TOWN_HALL  = 10;  // ok
+
+
+
+
 var TL = TL || {};
 
+var  building = "res/gui/Art/Buildings/";
+
 TL.PATH = {
-    building: "res/gui/Art/Buildings/"
+    army_camp: building + "army_camp/AMC_1_",
+    barrack: building + "barrack/BAR_1_",
+    builder_hut: building + "builder_hut",
+    clan_castle: building + "clan_castle/CLC_1_",
+    town_hall: building + "townhall/TOW_1_",
+    defense_base: building + "defense_base/DEF_",
+    laboratory: building + "labratory/LAB_1_",
+    obstacle: building + "obstacle/OBS_",
+
+    gold_storgae: building + "gold_storage/STO_1_",
+    elixir_storage: building + "elixir_storage/STO_2_",
+    dark_elixir_storage: building + "dark_elixir_storage/STO_3_",
+
+    gold_mine: building + "gold_mine/RES_1_",
+    elixir_collector: building + "elixir_collector/RES_2_",
+    dark_elixir_collector: building + "dark_elixir_collector/RES_3_",
+
+
+
 }
